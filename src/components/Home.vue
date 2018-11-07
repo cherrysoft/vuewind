777d6b1208ea0eebaaeb29e0c5963f4f8425b84e
Michael Quinn <michael@cherrysoft.com>
2018-11-07 16:16:24 +0100

    adding alert components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,66 @@
 <template>
 <div id="home">
-  <simple></simple>
-  <pill></pill>
-  <bordered></bordered>
-  <disabled></disabled>
-  <three-d></three-d>
-  <elevated></elevated>
-  <groups></groups>
-  <icons></icons>
+    <!-- buttons -->
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <simple></simple><pill></pill><bordered></bordered>
+              <disabled></disabled><three-d></three-d><elevated></elevated>
+              <groups></groups><icons></icons>
+          </div>
+      </div>
+    </div>
+    <!-- alerts -->
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <traditional></traditional>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <modern-badge></modern-badge>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <left-accent-border></left-accent-border>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <danger></danger>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <solid></solid>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <top-accent-border></top-accent-border>
+          </div>
+      </div>
+    </div>
+    <div class="flex mb-4">
+      <div class="container mx-auto">
+          <div class="w-full bg-grey-lightest h-12">
+              <banner></banner>
+          </div>
+      </div>
+    </div>
+
 </div>
 </template>
 
@@ -21,11 +74,21 @@ import ThreeD from '@/components/buttons/ThreeD'
 import Elevated from '@/components/buttons/Elevated'
 import Groups from '@/components/buttons/Groups'
 import Icons from '@/components/buttons/Icons'
+/* alerts */
+import Traditional from '@/components/alerts/Traditional'
+import ModernBadge from '@/components/alerts/ModernBadge'
+import LeftAccentBorder from '@/components/alerts/LeftAccentBorder'
+import Danger from '@/components/alerts/Danger'
+import Solid from '@/components/alerts/Solid'
+import TopAccentBorder from '@/components/alerts/TopAccentBorder'
+import Banner from '@/components/alerts/Banner'
 
 export default {
   name: 'Home',
   components: {
-    Simple, Pill, Bordered, Disabled, ThreeD, Elevated, Groups, Icons
+    Simple, Pill, Bordered, Disabled, ThreeD, Elevated, Groups, Icons,
+    Traditional, ModernBadge, LeftAccentBorder, Danger, Solid, TopAccentBorder, Banner
+
   }
 }
 </script>
